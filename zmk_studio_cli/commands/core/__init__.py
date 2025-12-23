d42f5bb29ff097ae77f1f9d9c2ac2cfb1ecffc33
Peter Johanson <peter@peterjohanson.com>
2025-12-23 10:59:18 -0700

    feat: Add support fow new core "changes" messages.
    Add support for check unsaved, save, and discard changes.

--- zmk_studio_cli/commands/core/__init__.py
+++ zmk_studio_cli/commands/core/__init__.py
@@ -11,12 +11,18 @@ from .get_device_info import core_get_device_info
 from .get_lock_state import core_get_lock_state
 from .lock import core_lock
 from .reset_settings import core_reset_settings
+from .check_unsaved_changes import core_check_unsaved_changes
+from .save_changes import core_save_changes
+from .discard_changes import core_discard_changes
 
 app = typer.Typer(name="core")
 app.command(name="get-device-info")(core_get_device_info)
 app.command(name="get-lock-state")(core_get_lock_state)
 app.command(name="lock")(core_lock)
 app.command(name="reset-settings")(core_reset_settings)
+app.command(name="check-unsaved-changes")(core_check_unsaved_changes)
+app.command(name="save-changes")(core_save_changes)
+app.command(name="discard-changes")(core_discard_changes)
 
 
 @app.callback()
